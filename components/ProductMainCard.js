39e3f87f472a054a6bf8359ae052ce83f1913d67
Michael-Xie <xiemicha@gmail.com>
2020-06-16 00:09:46 -0400

    Fix linting

--- components/ProductMainCard.js
+++ components/ProductMainCard.js
@@ -1,27 +1,42 @@
-import { Ionicons } from "@expo/vector-icons";
-import * as WebBrowser from "expo-web-browser";
-import * as React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { RectButton, ScrollView } from "react-native-gesture-handler";
-import { UserName } from "./TopBar/UserName";
-import { ActivityIndicator, Colors, Drawer } from "react-native-paper";
-import { Avatar, Button, Card, Title, Paragraph } from "react-native-paper";
+import { Ionicons } from '@expo/vector-icons';
+import * as WebBrowser from 'expo-web-browser';
+import * as React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { RectButton, ScrollView } from 'react-native-gesture-handler';
+import {
+  ActivityIndicator,
+  Colors,
+  Drawer,
+  Avatar,
+  Button,
+  Card,
+  Title,
+  Paragraph,
+} from 'react-native-paper';
 
-export default function ProductMainCard({price, totalVotes}) {
+import { t } from 'react-native-tailwindcss';
+import UserName from './TopBar/UserName';
+
+export default function ProductMainCard({ price, totalVotes }) {
   return (
-    <Card>
-      <Card.Content>
-        <Title>Card title</Title>
-        <Paragraph>Card content</Paragraph>
-      </Card.Content>
+    <Card style={[t.flex, t.flexCol, t.alignCenter]}>
       <Card.Actions>
         <Button icon="bookmark" />
       </Card.Actions>
-      <Card.Cover source={{ uri: "https://picsum.photos/700" }} />
-      <Card.Actions>
-        <Button icon="thumb-up" />
-        <Button icon="thumb-down" />
-      </Card.Actions>
+      <Card.Cover source={{ uri: 'https://picsum.photos/700' }} />
+      <Card.Content style={[t.flex, t.flexRow]}>
+        <View style={[]}>
+          <Text>{totalVotes} likes</Text>
+          <Card.Actions>
+            <Button icon="thumb-up" />
+            <Button icon="thumb-down" />
+          </Card.Actions>
+        </View>
+        <Card.Content style={[t.flex, t.flexRow]}>
+          <Text>${price.discounted}</Text>
+          <Text>${price.regular}</Text>
+        </Card.Content>
+      </Card.Content>
     </Card>
   );
 }
--- components/TopBar/UserName.js
+++ components/TopBar/UserName.js
@@ -1,15 +1,13 @@
-import * as React from "react";
-import { Text, View } from "react-native";
-import { Ionicons, FontAwesome5 } from "@expo/vector-icons";
+import * as React from 'react';
+import { Text, View } from 'react-native';
+import { Ionicons, FontAwesome5 } from '@expo/vector-icons';
 import { t } from 'react-native-tailwindcss';
 
-export function UserName({ status, children }) {
+export default function UserName({ status, children }) {
   return (
     <View style={[t.flex, t.flexRow, t.itemsCenter]}>
       <Text style={[t.p2]}>{children}</Text>
-      {status === "super" && (
-        <FontAwesome5 name="crown" size={16} color="black" />
-      )}
+      {status === 'super' && <FontAwesome5 name="crown" size={16} color="black" />}
     </View>
   );
 }
