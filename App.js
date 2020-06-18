ec0fb6ee8a5542f669044a14d0b1dac73516c1c9
Angela Lin <linyh0207@gmail.com>
2020-06-17 22:27:22 -0700

    Fix lint error

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import * as React from 'react';
-import { Platform, StatusBar, StyleSheet, View } from 'react-native';
-import { Provider as PaperProvider } from 'react-native-paper';
-
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import { Platform, StatusBar, View } from 'react-native';
+import { Provider as PaperProvider, Headline, Avatar } from 'react-native-paper';
+import { t } from 'react-native-tailwindcss';
+import {
+  createDrawerNavigator,
+  DrawerContentScrollView,
+  DrawerItemList,
+} from '@react-navigation/drawer';
 import useCachedResources from './hooks/useCachedResources';
 import BottomTabNavigator from './navigation/BottomTabNavigator';
 import LinkingConfiguration from './navigation/LinkingConfiguration';
@@ -20,6 +24,19 @@ function Root() {
   );
 }
 
+// Add drawer navigator header
+function CustomDrawerContent(props) {
+  return (
+    <DrawerContentScrollView {...props}>
+      <View style={[t.flex1, t.flexRow, t.p8, t.itemsCenter, t.bgGreen600]}>
+        <Headline style={[t.pR2]}>Username</Headline>
+        <Avatar.Icon size={24} icon="crown" />
+      </View>
+      <DrawerItemList {...props} />
+    </DrawerContentScrollView>
+  );
+}
+
 export default function App(props) {
   const isLoadingComplete = useCachedResources();
 
@@ -28,22 +45,37 @@ export default function App(props) {
   }
   return (
     <PaperProvider>
-      <View style={styles.container}>
+      <View style={[t.flex1, t.bgWhite]}>
         {Platform.OS === 'ios' && <StatusBar barStyle="dark-content" />}
         <NavigationContainer linking={LinkingConfiguration}>
-          <Drawer.Navigator initialRouteName="Home">
-            <Drawer.Screen name="Home" component={Root} />
-            <Drawer.Screen name="Root" component={Root} />
+          <Drawer.Navigator
+            initialRouteName="Home"
+            drawerContent={(items) => <CustomDrawerContent {...items} />}
+          >
+            <Drawer.Screen
+              name="My Posts"
+              component={Root}
+              options={{
+                drawerIcon: () => <Avatar.Icon size={24} icon="folder-multiple-image" />,
+              }}
+            />
+            <Drawer.Screen
+              name="My Achievements"
+              component={Root}
+              options={{
+                drawerIcon: () => <Avatar.Icon size={24} icon="trophy" />,
+              }}
+            />
+            <Drawer.Screen
+              name="Logout"
+              component={Root}
+              options={{
+                drawerIcon: () => <Avatar.Icon size={24} icon="logout" />,
+              }}
+            />
           </Drawer.Navigator>
         </NavigationContainer>
       </View>
     </PaperProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-});
