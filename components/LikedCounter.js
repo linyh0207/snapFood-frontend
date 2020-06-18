d997301978f61fc4130454f3ca106ae0cadc8b4b
Michael-Xie <xiemicha@gmail.com>
2020-06-17 20:08:27 -0400

    Fix bug where you can't have no votes once selected one

--- components/LikedCounter.js
+++ components/LikedCounter.js
@@ -1,47 +1,47 @@
 import React, { useState, useEffect } from 'react';
 
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
+import { t } from 'react-native-tailwindcss';
 import ToggleButton from './ToggleButton';
-import deconstructLike from '../helpers/deconstructLike';
 
-const LikedCounter = () => {
+const LikedCounter = (netLiked = 0) => {
   const [liked, setLiked] = useState(false);
   const [notLiked, setNotLiked] = useState(false);
-  console.log('liked', liked);
-  console.log('notLiked', notLiked);
-  //   useEffect(() => {
-  //     if (liked) {
-  //       setNotLiked(false);
-  //     } else {
-  //       setNotLiked(true);
-  //     }
 
-  //     if (notLiked) {
-  //       setLiked(true);
-  //     } else {
-  //       setLiked(false);
-  //     }
-  //   }, [liked, notLiked]);
+  useEffect(() => {
+    if (liked) {
+      setNotLiked(false);
+    }
+  }, [liked]);
+
+  useEffect(() => {
+    if (notLiked) {
+      setLiked(false);
+    }
+  }, [notLiked]);
   return (
-    <View>
-      <ToggleButton
-        selected={liked}
-        selectedIcon="thumb-up"
-        unselectedIcon="thumb-up-outline"
-        handleSelected={() => {
-          setLiked(!liked);
-          setNotLiked(liked);
-        }}
-      />
-      <ToggleButton
-        selected={notLiked}
-        selectedIcon="thumb-down"
-        unselectedIcon="thumb-down-outline"
-        handleSelected={() => {
-          setNotLiked(!notLiked);
-          setLiked(notLiked);
-        }}
-      />
+    <View style={[t.flex, t.flexCol]}>
+      {/* <Text>{netLiked} likes</Text> */}
+      <View style={[t.flex, t.flexRow, t.justifyStart]}>
+        <ToggleButton
+          selected={liked}
+          selectedIcon="thumb-up"
+          unselectedIcon="thumb-up-outline"
+          handleSelected={() => {
+            setLiked(!liked);
+            // setNotLiked(liked);
+          }}
+        />
+        <ToggleButton
+          selected={notLiked}
+          selectedIcon="thumb-down"
+          unselectedIcon="thumb-down-outline"
+          handleSelected={() => {
+            setNotLiked(!notLiked);
+            // setLiked(notLiked);
+          }}
+        />
+      </View>
     </View>
   );
 };
