f7549cf2614ccd88fb93d9143d3585d277c0868f
Angela Lin <linyh0207@gmail.com>
2020-06-17 17:33:53 -0700

    Add basic drawer navigator

--- App.js
+++ App.js
@@ -1,41 +1,49 @@
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-import * as React from "react";
-import { Platform, StatusBar, StyleSheet, View } from "react-native";
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import * as React from 'react';
+import { Platform, StatusBar, StyleSheet, View } from 'react-native';
 import { Provider as PaperProvider } from 'react-native-paper';
 
-import useCachedResources from "./hooks/useCachedResources";
-import BottomTabNavigator from "./navigation/BottomTabNavigator";
-import LinkingConfiguration from "./navigation/LinkingConfiguration";
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import useCachedResources from './hooks/useCachedResources';
+import BottomTabNavigator from './navigation/BottomTabNavigator';
+import LinkingConfiguration from './navigation/LinkingConfiguration';
 
 const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
 
+function Root() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Root" component={BottomTabNavigator} />
+    </Stack.Navigator>
+  );
+}
 
 export default function App(props) {
   const isLoadingComplete = useCachedResources();
 
   if (!isLoadingComplete) {
     return null;
-  } else {
-    return (
-      <PaperProvider>
-        <View style={styles.container}>
-          {Platform.OS === "ios" && <StatusBar barStyle="dark-content" />}
-          <NavigationContainer linking={LinkingConfiguration}>
-            <Stack.Navigator>
-              <Stack.Screen name="Root" component={BottomTabNavigator} />
-            </Stack.Navigator>
-          </NavigationContainer>
-        </View>
-      </PaperProvider>
-    );
   }
+  return (
+    <PaperProvider>
+      <View style={styles.container}>
+        {Platform.OS === 'ios' && <StatusBar barStyle="dark-content" />}
+        <NavigationContainer linking={LinkingConfiguration}>
+          <Drawer.Navigator initialRouteName="Home">
+            <Drawer.Screen name="Home" component={Root} />
+            <Drawer.Screen name="Root" component={Root} />
+          </Drawer.Navigator>
+        </NavigationContainer>
+      </View>
+    </PaperProvider>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: '#fff',
   },
 });
-
