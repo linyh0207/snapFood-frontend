35850ff23a894aae3e14124ac18c666c2b7c08f0
Michael-Xie <xiemicha@gmail.com>
2020-06-19 08:49:34 -0400

    Done rough format of achievement card (super basic) with achievement statement

--- screens/AchievementScreen.js
+++ screens/AchievementScreen.js
@@ -1,17 +1,22 @@
 import * as React from 'react';
 import { useState } from 'react';
-import { View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import { t } from 'react-native-tailwindcss';
-import { Modal, Portal, Text, TextInput, HelperText, Avatar } from 'react-native-paper';
+import { Modal, Portal, Text, TextInput, HelperText, Avatar, Button } from 'react-native-paper';
 import UserName from '../components/TopBar/UserName';
 
 export default function AchievementScreen() {
   return (
-    <View style={[t.flex, t.flexRow, t.itemsCenter, t.p2]}>
-      <Avatar.Icon icon="account" size={36} style={[t.p0]} />
-      <UserName styles={[t.pX1]} textStyles={[t.textLg]} status="super">
-        UserName
-      </UserName>
+    <View>
+      <View style={[t.flex, t.flexRow, t.itemsCenter, t.p2]}>
+        <Avatar.Icon icon="account" size={36} style={[t.p0]} />
+        <UserName styles={[t.pX1]} textStyles={[t.textLg]} status="super">
+          UserName
+        </UserName>
+      </View>
+      <Text style={[t.border, t.p3, t.m3, t.textCenter, t.textLg, t.fontBold, t.roundedLg]}>
+        This is a test
+      </Text>
     </View>
   );
 }
