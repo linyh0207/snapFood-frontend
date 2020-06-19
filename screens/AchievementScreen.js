2de89a35fa0d1e0eada8e3ebfa892be792aabe8a
Michael-Xie <xiemicha@gmail.com>
2020-06-19 00:12:00 -0400

    Update Username with styling props
    Add Username and profile icon to achievement screen

--- screens/AchievementScreen.js
+++ screens/AchievementScreen.js
@@ -2,8 +2,16 @@ import * as React from 'react';
 import { useState } from 'react';
 import { View } from 'react-native';
 import { t } from 'react-native-tailwindcss';
-import { Modal, Portal, Text, TextInput, HelperText } from 'react-native-paper';
+import { Modal, Portal, Text, TextInput, HelperText, Avatar } from 'react-native-paper';
+import UserName from '../components/TopBar/UserName';
 
 export default function AchievementScreen() {
-  return <></>;
+  return (
+    <View style={[t.flex, t.flexRow, t.itemsCenter, t.p2]}>
+      <Avatar.Icon icon="account" size={36} style={[t.p0]} />
+      <UserName styles={[t.pX1]} textStyles={[t.textLg]} status="super">
+        UserName
+      </UserName>
+    </View>
+  );
 }
