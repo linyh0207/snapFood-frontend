8802e5eddc029f1812bb7c87eb1c565ed7f195de
jon <jonathan-langlois@live.ca>
2020-06-21 21:02:25 -0700

    add search bar component

--- screens/ProductMainScreen.js
+++ screens/ProductMainScreen.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { View, Alert } from 'react-native';
+import { View } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
 import { useNavigation } from '@react-navigation/native';
 import { t } from 'react-native-tailwindcss';
@@ -7,6 +7,7 @@ import StyledButton from '../components/StyledButton';
 import DropDownMenu from '../components/DropDownMenu';
 import SnackBar from '../components/SnackBar';
 import ProductMainCard from '../components/ProductMainCard';
+import SearchBar from '../components/SearchBar';
 
 export default function ProductMainScreen() {
   // For account button to open drawer navigator
@@ -14,6 +15,7 @@ export default function ProductMainScreen() {
 
   return (
     <View style={[t.flex1, t.bgWhite]}>
+      <SearchBar searcher={1} latitude={5} longitude={10} radius={40000000} />
       <ScrollView contentContainerStyle={[t.p6]}>
         {/* Product card for product main page */}
         <ProductMainCard
