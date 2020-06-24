dbc6a794a51966e9fc4184ca38cbd08697d9f660
Michael-Xie <xiemicha@gmail.com>
2020-06-24 11:48:52 -0400

    Try getting current location after getting permission and show on screen

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
 import { StyleSheet, Text, View, Dimensions } from 'react-native';
 import * as Permissions from 'expo-permissions';
+import * as Location from 'expo-location';
 
 const styles = StyleSheet.create({
   container: {
@@ -12,13 +13,13 @@ const styles = StyleSheet.create({
   },
   mapStyle: {
     width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height,
+    height: Dimensions.get('window').height - 200,
   },
 });
 
 const markers = [{ latlng: { latitude: 37.7883, longitude: -122.432 }, title: 'T&T Supermarket' }];
 export default function MapScreen() {
-  const [location, setLocation] = useState({ latitude: null, longitude: null });
+  //   const [location, setLocation] = useState({ latitude: null, longitude: null });
 
   //   useEffect(async () => {
   //     const { status } = await Permissions.getAsync(Permissions.LOCATION);
@@ -31,25 +32,54 @@ export default function MapScreen() {
   //       (error) => console.log('Error', error)
   //     );
   //   }, []);
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
 
+  useEffect(() => {
+    (async () => {
+      const { status } = await Location.requestPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+      }
+      // contains object of timestamp: sec, mocked: boolean, coords: {altitude: int, heading: int, longitude: float, speed: float, latitude: float, accuracy: float}
+      const locationRaw = await Location.getCurrentPositionAsync({});
+      setLocation(locationRaw.coords);
+    })();
+  });
+
+  let text = 'Waiting..';
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify({ longitude: location.longitude, latitude: location.latitude });
+  }
+  if (location?.latitude) {
+    return (
+      <View style={styles.container}>
+        <Text>{text}</Text>
+        <MapView
+          showsUserLocation
+          style={styles.mapStyle}
+          provider={PROVIDER_GOOGLE}
+          initialRegion={{
+            latitude: location.latitude,
+            longitude: location.longitude,
+            // latitude: 37.78825,
+            // longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        >
+          {/* {markers.map((marker) => {
+            return <Marker coordinate={marker.latlng} title={marker.title} />;
+          })} */}
+        </MapView>
+      </View>
+    );
+  }
   return (
     <View style={styles.container}>
-      <MapView
-        style={styles.mapStyle}
-        provider={PROVIDER_GOOGLE}
-        initialRegion={{
-          //   latitude: location.latitude,
-          //   longitude: location.longitude,
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
-      >
-        {markers.map((marker) => {
-          return <Marker coordinate={marker.latlng} title={marker.title} />;
-        })}
-      </MapView>
+      <Text>We need your permission!</Text>
     </View>
   );
 }
