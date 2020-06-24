7029a807d66c6afb23c87e1388ba7fe8dcbfec63
Michael-Xie <xiemicha@gmail.com>
2020-06-24 10:32:05 -0400

    Add dummy marker and start on permission

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import MapView from 'react-native-maps';
+import React, { useState, useEffect } from 'react';
+import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
 import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import * as Permissions from 'expo-permissions';
 
 const styles = StyleSheet.create({
   container: {
@@ -15,10 +16,40 @@ const styles = StyleSheet.create({
   },
 });
 
+const markers = [{ latlng: { latitude: 37.7883, longitude: -122.432 }, title: 'T&T Supermarket' }];
 export default function MapScreen() {
+  const [location, setLocation] = useState({ latitude: null, longitude: null });
+
+  //   useEffect(async () => {
+  //     const { status } = await Permissions.getAsync(Permissions.LOCATION);
+
+  //     if (status !== 'granted') {
+  //       const response = await Permissions.askAsync(Permissions.LOCATION);
+  //     }
+  //     navigator.geolocation.getCurrentPosition(
+  //       ({ coords: { latitude, longitude } }) => setLocation({ latitude, longitude }),
+  //       (error) => console.log('Error', error)
+  //     );
+  //   }, []);
+
   return (
     <View style={styles.container}>
-      <MapView style={styles.mapStyle} />
+      <MapView
+        style={styles.mapStyle}
+        provider={PROVIDER_GOOGLE}
+        initialRegion={{
+          //   latitude: location.latitude,
+          //   longitude: location.longitude,
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      >
+        {markers.map((marker) => {
+          return <Marker coordinate={marker.latlng} title={marker.title} />;
+        })}
+      </MapView>
     </View>
   );
 }
