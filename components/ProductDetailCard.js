5f8761b7f007522bb89fd032fe54ce74342c2efb
Michael-Xie <xiemicha@gmail.com>
2020-06-26 10:33:54 -0400

    Add rough Map modal when user clicks on marker in ProductDetailCard

--- components/ProductDetailCard.js
+++ components/ProductDetailCard.js
@@ -2,6 +2,8 @@ import { Ionicons } from '@expo/vector-icons';
 import * as WebBrowser from 'expo-web-browser';
 import * as React from 'react';
 import { StyleSheet, Text, View, ImageBackground, Image } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
 import { RectButton, ScrollView } from 'react-native-gesture-handler';
 import {
   ActivityIndicator,
@@ -14,11 +16,14 @@ import {
   Paragraph,
   Chip,
   IconButton,
+  Portal,
+  Modal,
 } from 'react-native-paper';
 import { t } from 'react-native-tailwindcss';
 import ToggleButton from './ToggleButton';
 import UserName from './TopBar/UserName';
 import LikedCounter from './LikedCounter';
+import Map from './Map';
 
 const styles = StyleSheet.create({
   image: {
@@ -36,42 +41,51 @@ export default function ProductDetailCard({
 }) {
   const tags = ['bread', 'sliced'];
   const [bookmarked, setBookmarked] = React.useState(false);
+  const [showMapModal, setShowMapModal] = React.useState(false);
   return (
-    <View>
+    <SafeAreaView style={[t.flex1]}>
+      <Portal>
+        <Modal visible={showMapModal} onDismiss={() => setShowMapModal(!showMapModal)}>
+          <Map />
+        </Modal>
+      </Portal>
       <View>
-        <Text>19 hours ago</Text>
         <View>
-          <IconButton icon="map-marker" />
-          <Text>T&T Supermarket (500m)</Text>
+          <Text>19 hours ago</Text>
+          <View>
+            <IconButton icon="map-marker" onPress={() => setShowMapModal(!showMapModal)} />
+            <Text>T&T Supermarket (500m)</Text>
+          </View>
         </View>
-      </View>
-      <Image
-        style={{ width: '50%', height: '20%' }}
-        resizeMethod="resize"
-        resizeMode="center"
-        source={{ uri: 'https://picsum.photos/700' }}
-      />
-      <View>
-        <LikedCounter />
-        <ToggleButton
-          selected={bookmarked}
-          selectedIcon="bookmark"
-          unselectedIcon="bookmark-outline"
-          handleSelected={() => setBookmarked(!bookmarked)}
+        <Image
+          style={{ width: '50%', height: '20%' }}
+          resizeMethod="resize"
+          resizeMode="center"
+          source={{ uri: 'https://picsum.photos/700' }}
         />
-      </View>
-      <View>
         <View>
-          <Text>by: Amy</Text>
-          {tags.map((tag) => (
-            <Chip mode="outlined">{tag}</Chip>
-          ))}
+          <LikedCounter />
+          <ToggleButton
+            selected={bookmarked}
+            selectedIcon="bookmark"
+            unselectedIcon="bookmark-outline"
+            handleSelected={() => setBookmarked(!bookmarked)}
+          />
         </View>
-        <Text>
-          $1.50 <Text style={[t.lineThrough]}>$2.99</Text>
-        </Text>
+        <View>
+          <View>
+            <Text>by: Amy</Text>
+            {tags.map((tag) => (
+              // <Chip mode="outlined">{tag}</Chip>
+              <Text>{tag}</Text>
+            ))}
+          </View>
+          <Text>
+            $1.50 <Text style={[t.lineThrough]}>$2.99</Text>
+          </Text>
+        </View>
       </View>
-    </View>
+    </SafeAreaView>
   );
 }
 // <View>
