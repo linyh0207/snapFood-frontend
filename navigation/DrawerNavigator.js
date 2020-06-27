4aba36c3b1ed7cb40c1d7616d719e75cbd0c7515
Angela Lin <linyh0207@gmail.com>
2020-06-27 14:30:11 -0700

    Add bottomtabnavigator for MyPostsScreen and MyAchievementsScreen

--- navigation/DrawerNavigator.js
+++ navigation/DrawerNavigator.js
@@ -8,8 +8,6 @@ import {
   DrawerItemList,
 } from '@react-navigation/drawer';
 import BottomTabNavigator from './BottomTabNavigator';
-import AchievementScreen from '../screens/AchievementScreen';
-import MyPostsScreen from '../screens/MyPostsScreen';
 import OnboardingScreen from '../screens/OnboardingScreen';
 
 const Drawer = createDrawerNavigator();
@@ -39,14 +37,14 @@ export default function DrawerNavigator() {
       />
       <Drawer.Screen
         name="My Posts"
-        component={MyPostsScreen}
+        component={BottomTabNavigator}
         options={{
           drawerIcon: () => <Avatar.Icon size={24} icon="folder-multiple-image" />,
         }}
       />
       <Drawer.Screen
         name="My Achievements"
-        component={AchievementScreen}
+        component={BottomTabNavigator}
         options={{
           drawerIcon: () => <Avatar.Icon size={24} icon="trophy" />,
         }}
