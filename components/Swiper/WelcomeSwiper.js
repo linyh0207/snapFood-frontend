98fc4285679a984577e1cdb1a5e1dd97de40dc4d
Angela Lin <linyh0207@gmail.com>
2020-06-29 20:48:15 -0700

    Add logo to the onboarding screen

--- components/Swiper/WelcomeSwiper.js
+++ components/Swiper/WelcomeSwiper.js
@@ -1,15 +1,15 @@
 import React, { useState } from 'react';
-import { View, Text, Dimensions } from 'react-native';
+import { View, Text, Dimensions, Image } from 'react-native';
 import Carousel, { Pagination } from 'react-native-snap-carousel';
 import { MaterialIcons, FontAwesome5, FontAwesome } from '@expo/vector-icons';
 import { t } from 'react-native-tailwindcss';
-import { Headline } from 'react-native-paper';
+import logo from '../../assets/images/logo.png';
 
 export default function WelcomeSwiper() {
   const SCREEN_WIDTH = Dimensions.get('window').width;
 
   const Screen = ({ header, icon, description }) => (
-    <View style={[t.flex1, t.justifyCenter, t.itemsCenter]}>
+    <View style={[t.flex1, t.justifyCenter, t.itemsCenter, t.mX5]}>
       {icon}
       <Text style={[t.textWhite, t.text3xl, t.fontBold, t.mY4, t.textCenter]}>{header}</Text>
       <Text style={[t.textWhite, t.textLg, t.fontBold, t.mX8, t.textCenter]}>{description}</Text>
@@ -20,17 +20,17 @@ export default function WelcomeSwiper() {
     <Screen
       header="Snap"
       icon={<MaterialIcons name="photo-camera" size={60} color="white" />}
-      description="Snap a photo of discounted food item from your local grocery store"
+      description="Snap a photo of the discounted food item from your local grocery store."
     />,
     <Screen
       header="Find"
       icon={<FontAwesome name="search" size={60} color="white" />}
-      description="Find real-time discounted food items near you"
+      description="Find all the real-time discounted food deals near you."
     />,
     <Screen
       header="Help"
       icon={<FontAwesome5 name="hands-helping" size={60} color="white" />}
-      description="Help each other to reduce the food waste and food cost"
+      description="Help each other to reduce the food wastes and food costs."
     />,
   ];
 
@@ -38,7 +38,7 @@ export default function WelcomeSwiper() {
 
   return (
     <View style={[t.flex1, t.justifyCenter, t.itemsCenter, t.bgGreen600]}>
-      <Headline style={[t.pT8, t.fontBold, t.trackingWidest, t.textWhite]}>SNAPFOOD</Headline>
+      <Image source={logo} style={{ width: 305, height: 159 }} />
       <Carousel
         data={SCREENS}
         renderItem={({ item }) => item}
