5c77e5666333f24ee574939e8d30f7ebd4307cb0
Michael-Xie <xiemicha@gmail.com>
2020-07-01 16:43:18 -0400

    Start on fakeData

---
+++ utils/fakeData.js
@@ -0,0 +1,16 @@
+/* eslint-disable import/prefer-default-export */
+export const FAKE_HOME_LOCATIONS = {
+  Markham: { latitude: 43.847515, longitude: -79.294385 },
+  Victoria: { latitude: 48.428047, longitude: -123.366534 },
+};
+
+export const FAKE_STORE_LOCATIONS = {
+  Markham: { latitude: 43.874527, longitude: -79.285302 }, // Loblaws near Markville Mall
+  Victoria: { latitude: 48.4365402, longitude: -123.3610232 }, // Fairway Market
+};
+
+export const FAKE_USER = {
+  id: '5eead9d6d34bf31f58a86904',
+  name: 'Alice',
+  status: 'regular',
+};
